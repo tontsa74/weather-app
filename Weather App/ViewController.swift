fc5990fa1e3ed2dbe105b5ce933a00872acfe936
Toni Silventoinen <c7tsilve@kc408mac06.tamk.ad>
2019-10-07 08:49:42 +0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Toni Silventoinen on 07/10/2019.
+//  Copyright © 2019 Toni Silventoinen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
